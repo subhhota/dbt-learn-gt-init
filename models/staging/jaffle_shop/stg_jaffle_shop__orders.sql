357c18cebe69686103cbc2109a41994abc2c8c1d
Subh Hota <h2a.s6u@gmail.com>
2026-02-23 16:56:57 -0500

    added source

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -3,6 +3,8 @@
         id as order_id,
         user_id as customer_id,
         order_date,
-        status
+        status,
+        _etl_loaded_at
 
-    from raw.jaffle_shop.orders
+
+    from {{source('jaffle_shop', 'orders')}}
